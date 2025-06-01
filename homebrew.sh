97e9641d6230c4099b31914e87004e03fd671399
Kazuki Furukawa <6816398+karur4n@users.noreply.github.com>
2025-06-01 15:32:42 +0900

    update

---
+++ homebrew.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+brew install starship
+
+brew install eza
+brew install zoxide
+
+brew install git
+
+brew install fzf
+
+brew install mise
+
+brew install ghq
+
+brew install rg
+
+brew install gh
+
+brew install gibo
+
+brew install mas
+
+## Cask
+
+brew install google-chrome
+
+brew install font-udev-gothic
+
+brew install raycast
+
+brew install wezterm
+brew install iterm2
+
+brew install visual-studio-code
+brew install cursor
+
+brew install claude
+
+brew install obsidian
+brew install logseq
+
+brew install aqua-voice
+brew install superwhisper
+
+brew install monitorcontrol
+
+brew install fork
+
+brew install tableplus
+
+brew install popclip
